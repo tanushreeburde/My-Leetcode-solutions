c64b874edd06768ec149991fa158b87315dcd69a
Tanushree🌿| Learner <tanushreeburde2208@gmail.com>
2025-08-09 18:32:34 +0530

    Update Palindrome Number.java

--- Easy/Palindrome Number.java
+++ Easy/Palindrome Number.java
@@ -1,3 +1,13 @@
+/*Problem Statement: Given an integer x, return true if x is a palindrome, and false otherwise.*/
+//Link: https://leetcode.com/problems/palindrome-number/description/
+//
+//Approach:
+// 1. Used modulus formula to get each digit from the x and stored in rev using a formula such that we get reversed x in rev.
+// 2. Then compared rev with x value that was stored in ori variable and also put condition for non-postive numbers.
+//
+// Complexity:
+// Time: O(log(n))
+// Memory: O(1)
 
 class Solution {
     public boolean isPalindrome(int x) {
